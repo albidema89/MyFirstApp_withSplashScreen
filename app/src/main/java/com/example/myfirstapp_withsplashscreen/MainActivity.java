6c9b0e78cdb26ad5e2aed2a198944a6b3fd7cff0
albidema89 <albidema@gmail.com>
2017-02-05 20:51:34 +0100

    Cleaned the code. Now finishing the activity when starting an update. Now updating last update text view at every resume.

--- app/src/main/java/com/example/myfirstapp_withsplashscreen/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp_withsplashscreen/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myfirstapp_withsplashscreen;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,7 +19,6 @@ import java.util.Collections;
 public class MainActivity extends AppCompatActivity {
 
     public LinearLayout parent;
-    //public RelativeLayout parent;
 
     ArrayAdapter<String> leagues_spinnerAdapter;
     Spinner leagues_sp;
@@ -28,13 +28,14 @@ public class MainActivity extends AppCompatActivity {
     public static int league_selected;
     public static String team_selected;
 
+    TextView last_update_view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Intent intent = getIntent();
         ArrayList<String> leagues_array = intent.getStringArrayListExtra("leagues");
-        String last_update = intent.getStringExtra("last_update");
 
         for(int i=0; i<SplashScreen.full_teams_array.size(); i++){
             Collections.sort(SplashScreen.full_teams_array.get(i), String.CASE_INSENSITIVE_ORDER);
@@ -45,21 +46,10 @@ public class MainActivity extends AppCompatActivity {
         parent.setOrientation(LinearLayout.VERTICAL);
         parent.setLayoutParams(param);
 
-        //RelativeLayout.LayoutParams param = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-        //parent = new RelativeLayout(this);
-        //parent.setLayoutParams(param);
-
         setContentView(parent);
-/*
-        Intent intent = new Intent(this, DownloadService.class);
-        intent.putExtra("url", "http://www.fipav.pavia.it/calendari.asp");
-        startService(intent);
-*/
 
         // preparazione dello Spinner per mostrare l'elenco delle squadre
         teams_spinnerAdapter = new ArrayAdapter<String>(this, R.layout.spinner_row);
-        //param = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        //param.addRule(RelativeLayout.CENTER_IN_PARENT);
         teams_sp = new Spinner(this);
         teams_sp.setLayoutParams(param);
         teams_sp.setAdapter(teams_spinnerAdapter);
@@ -69,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> arg0, View arg1,
                                        int arg2, long arg3) {
                 TextView txt = (TextView) arg1.findViewById(arg1.getId());
-                //invio.setText(MainActivity.leagues_sp.getSelectedItem().toString() +" - " +MainActivity.teams_sp.getSelectedItem().toString());
             }
 
             @Override
@@ -79,9 +68,6 @@ public class MainActivity extends AppCompatActivity {
 
         // preparazione dello Spinner per mostrare l'elenco dei campionati
         leagues_spinnerAdapter = new ArrayAdapter<String>(this, R.layout.spinner_row);
-        //param = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        //param.addRule(RelativeLayout.CENTER_IN_PARENT);
-        //param.addRule(RelativeLayout.ABOVE, teams_sp.getId());
         leagues_sp = new Spinner(this);
         leagues_sp.setLayoutParams(param);
         leagues_sp.setAdapter(leagues_spinnerAdapter);
@@ -89,16 +75,7 @@ public class MainActivity extends AppCompatActivity {
         {
             @Override
             public void onItemSelected(AdapterView<?> arg0, View arg1,
-                                       int arg2, long arg3) {/*
-                TextView txt = (TextView) arg1.findViewById(arg1.getId());
-                String s = txt.getText().toString();
-                String s_mod = s.replace(" ", "%20");
-
-                Intent intent_teams = new Intent(MainActivity.this, DownloadTeamsService.class);
-                intent_teams.putExtra("schedule_url", "http://www.fipav.pavia.it/calendari.asp?id=Camp&scelta=" +s_mod);
-                intent_teams.putExtra("ranking_url", "http://www.fipav.pavia.it/classifica.asp?id=Camp&scelta=" +s_mod);
-                startService(intent_teams);*/
-
+                                       int arg2, long arg3) {
                 teams_spinnerAdapter.clear();
                 for(int i=0; i<SplashScreen.full_teams_array.get(leagues_sp.getSelectedItemPosition()).size(); i++) {
                     teams_spinnerAdapter.add(SplashScreen.full_teams_array.get(leagues_sp.getSelectedItemPosition()).get(i).toString());
@@ -119,17 +96,14 @@ public class MainActivity extends AppCompatActivity {
         parent.addView(teams_sp);
 
         final Button invio = new Button(this);
-        //param = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        //param.removeRule(RelativeLayout.ABOVE);
-        //param.addRule(RelativeLayout.BELOW, teams_sp.getId());
         invio.setLayoutParams(param);
         invio.setText("INVIO");
 
         parent.addView(invio);
 
-        TextView last_update_view = new TextView(this);
+        last_update_view = new TextView(this);
         last_update_view.setLayoutParams(param);
-        last_update_view.setText(last_update);
+        last_update_view.setText(SplashScreen.last_update);
         parent.addView(last_update_view);
 
         invio.setOnClickListener(new View.OnClickListener() {
@@ -151,11 +125,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Perform action on click
                 startUpdateService(v);
-                //league_selected = leagues_sp.getSelectedItemPosition();
-                //team_selected = teams_sp.getSelectedItem().toString();
-                update.setClickable(false);
-                invio.setClickable(false);
-                update.setText("ATTENDERE ...");
+                finish();
             }
         });
     }
@@ -168,33 +138,15 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user clicks the Update button */
     public void startUpdateService (View view) {
-        //Intent intent = new Intent(this, UpdateService.class);
-        //intent.setAction("com.example.myfirstapp_withsplashscreen.UpdateService");
-        //startService(intent);
         Intent intent= new Intent();
         intent.setAction("com.example.myfirstapp_withsplashscreen.SINGLE_UPDATE");
-        //intent.putExtra(MainActivity.INTENT_EXTRA, "Additional info");
         sendBroadcast(intent);
-    }
-/*
-    private MyTestReceiver mReceiver;
-    private TeamsReceiver TeamsReceiver;
-
-    @Override
-    protected void onPause() {
-        unregisterReceiver(mReceiver);
-        unregisterReceiver(TeamsReceiver);
-        Log.d("myTag", "qui è scattato onPause");
-        super.onPause();
     }
 
     @Override
     protected void onResume() {
-        mReceiver=new MyTestReceiver(this);
-        registerReceiver( mReceiver, new IntentFilter(INTENT_ACTION));
-        TeamsReceiver=new TeamsReceiver(this);
-        registerReceiver( TeamsReceiver, new IntentFilter(INTENT_DOWNLOAD_TEAMS));
+        last_update_view.setText(SplashScreen.last_update);
         Log.d("myTag", "qui è scattato onResume");
         super.onResume();
-    }*/
+    }
 }
