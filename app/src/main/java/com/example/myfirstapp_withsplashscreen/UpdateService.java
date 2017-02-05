6e53d01e68c9dd5239a091cc274d157733bebade
albidema89 <albidema@gmail.com>
2017-02-05 20:58:04 +0100

    Changed period from 20 minutes to 30 minutes (it does not seems to work properly anyway).

--- app/src/main/java/com/example/myfirstapp_withsplashscreen/UpdateService.java
+++ app/src/main/java/com/example/myfirstapp_withsplashscreen/UpdateService.java
@@ -6,25 +6,43 @@ import android.content.Intent;
 import android.util.Log;
 
 public class UpdateService extends JobService {
+
+    //private UpdateAsyncTask mUpdateAsyncTask = null;
+
     @Override
-    public boolean onStartJob(JobParameters params) {
+    public boolean onStartJob(final JobParameters params) {
 
         Log.v("UpdateServiceAtBoot", "UpdateServiceAtBoot -- onStartJob()");
 
         Intent i = new Intent(getApplicationContext(), UpdateIntentService.class);
         i.setAction("UpdateIntent.SCHEDULED_UPDATE");
         getApplicationContext().startService(i);
-
         jobFinished(params, false);
-
+/*
+        mUpdateAsyncTask = new UpdateAsyncTask(this) {
+            @Override
+            protected void onPostExecute(Boolean success) {
+                Log.v("UpdateServiceAtBoot", "UpdateServiceAtBoot -- calling jobFinished");
+                jobFinished(params, false);
+            }
+        };
+        mUpdateAsyncTask.execute();
+*/
         // From Android guidelines:
         // True if your service needs to process the work (on a separate thread). False if there's no more work to be done for this job.
-        return false;
+        return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
+
+        Log.v("UpdateServiceAtBoot", "UpdateServiceAtBoot -- onStopJob()");
+        /*
         // true if we'd like to be rescheduled
+        if (mUpdateAsyncTask != null) {
+            mUpdateAsyncTask.cancel(true);
+        }
+        */
         return true;
     }
 
